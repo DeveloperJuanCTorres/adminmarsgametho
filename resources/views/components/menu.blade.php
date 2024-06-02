8af3b520972456db391f7c862163ab55facb158d
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2024-06-02 10:51:24 -0500

    cambios menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -24,7 +24,7 @@
         <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
             <i class="icon-columns menu-icon"></i>
-            <span class="menu-title">Form elements</span>
+            <span class="menu-title">Gestion Juegos</span>
             <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="form-elements">
@@ -36,7 +36,7 @@
         <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
             <i class="icon-bar-graph menu-icon"></i>
-            <span class="menu-title">Charts</span>
+            <span class="menu-title">Gestion Ventas</span>
             <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="charts">
@@ -48,7 +48,7 @@
         <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
             <i class="icon-grid-2 menu-icon"></i>
-            <span class="menu-title">Tables</span>
+            <span class="menu-title">Gestion Cobros</span>
             <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="tables">
@@ -60,7 +60,7 @@
         <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
             <i class="icon-contract menu-icon"></i>
-            <span class="menu-title">Icons</span>
+            <span class="menu-title">Gestion Comisiones</span>
             <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="icons">
@@ -72,7 +72,7 @@
         <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
             <i class="icon-head menu-icon"></i>
-            <span class="menu-title">User Pages</span>
+            <span class="menu-title">Gestion Trafico</span>
             <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="auth">
@@ -81,26 +81,7 @@
             <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
             </ul>
         </div>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
-            <i class="icon-ban menu-icon"></i>
-            <span class="menu-title">Error pages</span>
-            <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="error">
-            <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
-            </ul>
-        </div>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="pages/documentation/documentation.html">
-            <i class="icon-paper menu-icon"></i>
-            <span class="menu-title">Documentation</span>
-        </a>
-        </li>
+        </li>        
     </ul>
     </nav>
     <!-- partial -->
